472e78a15e006686a83c081dfaa8f15cb00b04b7
Johannes_Martin <alpha.aardvark1@gmail.com>
2023-11-29 17:51:17 -0500

    more GUI updates

--- Actuator code v1.0/experimentWithGUI.py
+++ Actuator code v1.0/experimentWithGUI.py
@@ -1,42 +1,42 @@
-# from tkinter import *
+from tkinter import *
 
-# root = Tk()
-# root.title("Testing")
+root = Tk()
+root.title("Testing")
 
-# def myClick():
-#     myLabel = Label(root, text = "Christmas!!", fg = "blue")
-#     myLabel.pack()
+def myClick():
+    myLabel = Label(root, text = "Christmas!!", fg = "blue")
+    myLabel.pack()
 
-# myButton = Button(root, text = "Buttttooooonnn", padx = 30, pady = 20, command = myClick, fg = "red", bg = "green")
-# myButton.pack()
+myButton = Button(root, text = "Buttttooooonnn", padx = 30, pady = 20, command = myClick, fg = "red", bg = "green")
+myButton.pack()
 
 
-# root.mainloop()
+root.mainloop()
 
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
-# print(plt.syle.available)
-plt.style.use('fivethirtyeight')
+# # print(plt.syle.available)
+# plt.style.use('classic')
 
-ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
-dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
+# ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+# dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
 
-py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+# py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
 
-js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
+# js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
 
-plt.plot(ages_x, py_dev_y, color = 'b', marker = 'o', linewidth = 3, label = 'Python')
-plt.plot(ages_x, js_dev_y, color = 'r', marker = '.', linewidth = 3, label = 'JavaScript')
-plt.plot(ages_x, dev_y, color = '#444444', linestyle = '--', label = 'All Devs')
+# plt.plot(ages_x, py_dev_y, color = 'b', marker = 'o', linewidth = 3, label = 'Python')
+# plt.plot(ages_x, js_dev_y, color = 'r', marker = '.', linewidth = 3, label = 'JavaScript')
+# plt.plot(ages_x, dev_y, color = '#444444', linestyle = '--', label = 'All Devs')
 
-plt.xlabel('Ages (years)')
-plt.ylabel('Median Salary (USD)')
-plt.title('Median Salary (USD) by Age')
+# plt.xlabel('Ages (years)')
+# plt.ylabel('Median Salary (USD)')
+# plt.title('Median Salary (USD) by Age')
 
-plt.legend()
+# plt.legend()
 
-plt.grid(True)
+# plt.grid(True)
 
-plt.tight_layout()
+# plt.tight_layout()
 
-plt.show()
+# plt.show()
