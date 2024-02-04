be8fbce568e79f786cb5343ddeea0a3b8b6a8736
Johannes_Martin <alpha.aardvark1@gmail.com>
2024-02-04 13:41:39 -0500

    Trying to figure out websockets

--- Actuator code v1.0/definitionTesting.py
+++ Actuator code v1.0/definitionTesting.py
@@ -2,6 +2,8 @@ from labjack import ljm
 import numpy as np
 import msvcrt
 import time
+import websockets
+import asyncio
 from QDAC_Class import *
 
 handle = ljm.openS("T7","ANY","ANY")
@@ -13,22 +15,44 @@ handle = ljm.openS("T7","ANY","ANY")
 
 # print(testSensor.data)
 
+
+#Websocket connection code
+
+
+
+# def sendData():
+#      with connect("ws://localhost:3000/") as websocket:
+#           websocket.send(testLoadCell.data[-1])
+#           message = websocket.recv()
+#           print("Recieved: ", message)
+
+
+
+
+
+
+
+
+#Load cell accquire data funcitons
 testLoadCell = sensor(handle, "AIN0", True)
 
-def acquireDataTimed(duration, interval): #duration and interval in seconds
+def acquireDataTimed(duration, interval): #duration is overall duration of measuring data, and interval is how often it reads and stores data, both are in seconds
         startTime = time.time()
         endTime = startTime + duration
 
         while time.time() < endTime:
             testLoadCell.takeData()
-            print(testLoadCell.data[-1])
+            # print(testLoadCell.data[-1])
+            # sendData()
             time.sleep(interval)
+            return testLoadCell.data[-1]
 
 def acquireDataIndefinite(interval):
-     try:
+    try:
           while(True):
             testLoadCell.takeData()
             print(testLoadCell.data[-1])
+            # sendData()
             time.sleep(interval)
     except KeyboardInterrupt:
         print("Data acquisition was manually halted")
@@ -37,5 +61,21 @@ def printAllLoadTestData():
     for i in range(len(testLoadCell.data)):
         print(testLoadCell.data[i])
 
+
+
 testLoadCell.wipeData()
+
+async def communicateWithServer():
+     url = "wss://api.golioth.io/v1/ws/"
+
+     async with websockets.connect(url) as ws:
+                  
+        await ws.send(acquireDataTimed(10, 0.01))
+        while True:
+            recvText = await ws.recv()
+            print("> {}".format(recvText))
+
+
+asyncio.get_event_loop().run_until_complete(communicateWithServer())
+
 acquireDataTimed(10, 0.01) #arbitrary time values
